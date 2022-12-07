872b408f0e17cc14a86895cea10452c428c9b6ec
saito <tukaimazero@gmail.com>
2022-12-07 20:42:34 +0900

    top page JavaScript

---
+++ Progress_management.js
@@ -0,0 +1,12 @@
+window.addEventListener("load",() =>{
+
+    document.getElementById("user_register").addEventListener("click",() =>{
+        document.getElementById("newUser").style["display"] = "block";
+        document.getElementById("user_register").style["display"] = "none";
+    })
+
+
+    
+    
+})
+
